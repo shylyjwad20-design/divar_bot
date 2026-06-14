ec3da24f9b199a59a9638dfd875ff01afd364f50
shylyjwad20-design <shylyjwad20@gmail.com>
2026-06-14 16:28:14 +0330

    Update main.py

--- main.py
+++ main.py
@@ -8,22 +8,24 @@ BOT_TOKEN = "8721044060:AAH5XALKBtG_0SBP2XDLY0Oee4Z0NGc2u7I"
 CHANNEL_ID = "@aaasd62"
 
 bot = telebot.TeleBot(BOT_TOKEN)
-
 seen_tokens = set()
 
 def get_divar_ads():
-    url = "https://api.divar.ir/v8/web-search/dezful/car"
-    headers = {"User-Agent": "Mozilla/5.0"}
+    url = "https://api.divar.ir/v8/web-search/dezful/light-cars-and-vans"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 Chrome/120.0.0.0 Mobile Safari/537.36",
+        "x-standard-divar-error": "true",
+        "x-divar-web-version": "4.0.0"
+    }
     
     try:
         response = requests.get(url, headers=headers, timeout=10)
         data = response.json()
         
         widget_list = data.get("widget_list", [])
+        print(f"تعداد آیتم‌ها: {len(widget_list)}")
         
-        # چاپ کامل اولین آیتم برای دیدن ساختار
         if widget_list:
-            print("ساختار آیتم اول:")
             print(json.dumps(widget_list[0], ensure_ascii=False, indent=2))
         
         return []
